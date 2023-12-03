176857ce2f7b6f9cbb55ff8de51f5bb41bf17e22
Juampi27 <jpbarnetche27@gmail.com>
2023-12-03 13:48:57 -0300

    capitulo 1, 2, 3 y 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { montserrat } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        <h1>Esto es parte del layout!</h1>
+        {children}
+        <footer className='py-10 flex justify-center item-center'>Hecho por Vercel</footer>
+      </body>
     </html>
   );
 }
